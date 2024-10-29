db6f122c532d7bd5228abdb0a8fbbfb95b12ec14
Egor Martynov <sahahod2004@gmail.com>
2024-10-29 09:59:13 +0300

    add main

---
+++ main.py
@@ -0,0 +1,8 @@
+# main.py - Главная точка входа для приложения Library Management System
+
+def main():
+    print("Welcome to the Library Management System!")
+    # TODO: Initialize and run the application
+
+if __name__ == "__main__":
+    main()
